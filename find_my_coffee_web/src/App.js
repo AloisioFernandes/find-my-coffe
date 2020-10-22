47f061a6ba8e1ef286d40e75b6c4b0768136d473
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-10-22 13:04:07 -0300

    Front end web ReactJS concluido

--- find_my_coffee_web/src/App.js
+++ find_my_coffee_web/src/App.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
+import Establishment from './components/Establishment'
+import NearestCoffees from './components/NearestCoffees'
+
 import EstablishmentsService from './services/establishments_service'
 
 function App() {
   const [latitude, setLatitude] = useState(0)
   const [longitude, setLongitude] = useState(0)
   const [location, setLocation] = useState([])
+  const [selected, setSelected] = useState({})
 
   const { REACT_APP_GOOGLE_API_KEY } = process.env
 
@@ -41,13 +45,23 @@ function App() {
               return (
                 <Marker key={index} icon="/images/coffee-pin.png" title={item.name} animation="4" 
                   position={{lat: item.geometry.location.lat, lng: item.geometry.location.lng}}
+                  onClick={() => setSelected(item)}
                 />
               )
             })
           }
+          {
+            selected.place_id && (
+              <Establishment place={selected} />
+            )
+          }
           <Marker key="my location" icon="/images/my-location-pin.png" title="Seu local" animation="2"
             position={{lat: latitude, lng: longitude}}
           />
+
+          {(latitude !== 0 && longitude !== 0) && 
+            <NearestCoffees latitude={latitude} longitude={longitude} />
+          }
 
         </GoogleMap>
       </LoadScript>      
--- find_my_coffee_web/src/components/Establishment/index.js
+++ find_my_coffee_web/src/components/Establishment/index.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import EstablishmentsService from '../../services/establishments_service'
+import Ratings from './Ratings'
 
 import styled from 'styled-components'
 
@@ -50,11 +51,29 @@ const Establishment = (props) => {
       {
         (establishment.photos) ?
         <Image src={`
-        https://maps.googleapis.com/maps/api/place/photo?photoreference=${establishment.photos[0].photo_reference}&key=${REACT_APP_GOOGLE_API_KEY}
+        https://maps.googleapis.com/maps/api/place/photo?photoreference=${establishment.photos[0].photo_reference}&key=${REACT_APP_GOOGLE_API_KEY}&maxwidth=400
         `} alt="Coffee Photo" 
         />
         : <Image src="/images/no_photo.jpg" alt="Coffee no Photo" />
       }
+      <Title>{establishment.name}</Title>
+      {
+        (establishment.opening_hours) ?
+          <div>
+            { (establishment.opening_hours.open_now === true) ? 'Aberto' : 'Fechado' }
+            <hr />
+            {
+              establishment.opening_hours.weekday_text.map((schedule, index) => {
+                return (<Paragraph key={index}>{schedule}</Paragraph>)
+              })
+            }
+          </div>
+          : <Paragraph>Não há cadastro de dias e horários abertos</Paragraph>
+      }
+      <hr />
+      <Paragraph>{establishment.formatted_address}</Paragraph>
+
+      <Ratings place={props.place} />
     </LeftBar>
   )
 }
